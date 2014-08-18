144fde434b49f93bde1be3d28b244dc1d0ca95c9
andreweber <afcorreoprivado@gmail.com>
2014-08-18 14:16:12 -0500

    maven

--- freepay/src/main/java/co/com/freepay/Cuenta.java
+++ freepay/src/main/java/co/com/freepay/Cuenta.java
@@ -17,8 +17,8 @@ public class Cuenta {
     /**
      * @param numeroCuenta numero de la cuenta.
      */
-    public Cuenta(final long numeroCuenta) {
-        numero = numeroCuenta;
+    public Cuenta(String numeroCuenta) {
+        numero = Long.valueOf(numeroCuenta);
     }
 
     /**
