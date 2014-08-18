216faad563776bc9023053b6347ee26efb1dd143
andreweber <afcorreoprivado@gmail.com>
2014-08-18 12:02:26 -0500

    ajustes analisis estatico

--- freepay/src/main/java/co/com/freepay/Cuenta.java
+++ freepay/src/main/java/co/com/freepay/Cuenta.java
@@ -5,59 +5,59 @@ package co.com.freepay;
  */
 public class Cuenta {
 
-	/**
-	 * numero de la cuenta.
-	 */
-	private long numero;
-	/**
-	 * saldo disponible en la cuenta.
-	 */
+    /**
+     * numero de la cuenta.
+     */
+    private long numero;
+    /**
+     * saldo disponible en la cuenta.
+     */
     private long saldo;
 
-	/**
-	 * @param numeroCuenta
-	 */
-	public Cuenta(final long numeroCuenta) {
-		numero = numeroCuenta;
-	}
+    /**
+     * @param numeroCuenta numero de la cuenta.
+     */
+    public Cuenta(final long numeroCuenta) {
+        numero = numeroCuenta;
+    }
 
-	/**
-	 * @return numero de la cuenta.
-	 */
-	public final long getNumero() {
-		return numero;
-	}
+    /**
+     * @return numero de la cuenta.
+     */
+    public final long getNumero() {
+        return numero;
+    }
 
-	/**
-	 * @return saldo de la cuenta.
-	 */
-	public final long getSaldo() {
-		return saldo;
-	}
+    /**
+     * @return saldo de la cuenta.
+     */
+    public final long getSaldo() {
+        return saldo;
+    }
 
-	/**
-	 * @param monto
-	 * @throws Exception
-	 */
-	public final void retirar(long monto) throws Exception {
-		if(monto < 0) {
-			throw new Exception("El monto a retirar no puede ser negativo.");
-		}
-		if(monto < saldo) {
-			saldo = saldo - monto;
-		} else {
-			throw new Exception("Saldo insuficiente para realizar el retiro.");
-		}
-	}
+    /**
+     * @param monto valor a retirar.
+     * @throws Exception en caso de no poder hacer el retiro.
+     */
+    public final void retirar(long monto) throws Exception {
+        if(monto < 0) {
+            throw new Exception("El monto a retirar no puede ser negativo.");
+        }
+        if(monto < saldo) {
+            saldo = saldo - monto;
+        } else {
+            throw new Exception("Saldo insuficiente para realizar el retiro.");
+        }
+    }
 
-	/**
-	 * @param monto
-	 * @throws Exception
-	 */
-	public final void consignar(long monto) throws Exception {
-		if(monto < 0) {
-			throw new Exception("El monto a consignar no puede ser negativo.");
-		}
-		saldo = saldo + monto;
-	}
+    /**
+     * @param monto valor a consignar.
+     * @throws Exception en caso de no poder hacer la consignación.
+     */
+    public final void consignar(long monto) throws Exception {
+        if(monto < 0) {
+            throw new Exception("El monto a consignar no puede ser negativo.");
+        }
+        saldo = saldo + monto;
+    }
 }
