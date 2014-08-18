87184a649ed562a9c31044fa38e16bd3cf95a1bf
andreweber <afcorreoprivado@gmail.com>
2014-08-18 11:14:54 -0500

    Se agrego el javadoc.

--- freepay/src/main/java/co/com/freepay/Cuenta.java
+++ freepay/src/main/java/co/com/freepay/Cuenta.java
@@ -1,22 +1,44 @@
 package co.com.freepay;
 
+/**
+ * 
+ * @author ANDRES
+ *
+ */
 public class Cuenta {
 	
 	private long numero;
 	private long saldo;
 	
+	/**
+	 * 
+	 * @param numero
+	 */
 	public Cuenta(long numero) {
 		this.numero = numero;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public long getNumero() {
 		return numero;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public long getSaldo() {
 		return saldo;
 	}
 	
+	/**
+	 * 
+	 * @param monto
+	 * @throws Exception
+	 */
 	public void retirar(long monto) throws Exception {
 		if(monto < 0) {
 			throw new Exception("El monto a retirar no puede ser negativo.");
@@ -29,6 +51,11 @@ public class Cuenta {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param monto
+	 * @throws Exception
+	 */
 	public void consignar(long monto) throws Exception {
 		if(monto < 0) {
 			throw new Exception("El monto a consignar no puede ser negativo.");
